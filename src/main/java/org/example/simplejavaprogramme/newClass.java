d978a89e2dcd383a7193622c36953b582cb5cbf9
ruman <rumanmn80@gmail.com>
2024-09-07 12:47:28 +0530

    "1st commuit"

---
+++ src/main/java/org/example/simplejavaprogramme/newClass.java
@@ -0,0 +1,13 @@
+package org.example.simplejavaprogramme;
+
+public class newClass {
+    int i=10;
+
+    public static void main(String[] args) {
+        newClass newClass = new newClass();
+       System.out.println(newClass.i);
+       System.out.println("25");
+       //hii
+
+    }
+}
